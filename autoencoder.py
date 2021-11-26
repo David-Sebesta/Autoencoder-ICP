fcce5f19ba2c0ebfe229de9c50b590410289dd98
David-Sebesta-FIT <64850214+David-Sebesta-FIT@users.noreply.github.com>
2021-11-26 18:29:58 -0500

    Better Data Loading
    Added 8x8, 28x28 digit loading
    Added Yale Faces loading

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class autoencoder:
@@ -11,54 +12,128 @@ class autoencoder:
         self.transformed_data = None
         self.decoded_data = None
 
+        self.e_val = None
+        self.e_vec = None
+
+        self.type = None
+
         pass
 
-    def pca_train(self, samples, p, H=0):
+    def pca_train(self, samples, p, h=0):
         """
         :param samples:
         :param p: Total Variance Explanied between 0 and 1
+        :param h
         """
 
-        self.sample_mean = samples.mean()                            # Compute Sample mean
-        centered_samples = samples - self.sample_mean                # Center samples around mean
+        self.type = 'PCA'
 
-        sample_cov = np.cov(centered_samples, rowvar=False)     # Find sample covariance matrix
+        self.sample_mean = samples.mean()  # Compute Sample mean
+        centered_samples = samples - self.sample_mean  # Center samples around mean
 
-        e_val, e_vec = sorted_eigen(sample_cov)                 # Eigen Decomposition of sample covariance matrix
-                                                                # They have to be sorted in decending order
+        sample_cov = np.cov(centered_samples, rowvar=False)  # Find sample covariance matrix
 
-        print(np.sum(e_val))
+        e_val, e_vec = sorted_eigen(sample_cov)  # Eigen Decomposition of sample covariance matrix
+        # They have to be sorted in descending order
 
-        if H == 0:
+        if h == 0:
             sum = e_val[0]
-            stop_val = p*np.trace(sample_cov)
-            H = 0
+            stop_val = p * np.trace(sample_cov)
+            h = 0
             while sum < stop_val:
-                H += 1
-                sum += e_val[H]
-            print(H+1)
+                h += 1
+                sum += e_val[h]
 
-        self.H = H
+        self.H = h
 
-        self.principle_components = e_vec[:, 0:H]
+        print("The number of principle components is:", self.H)
 
-    def encode(self, test_samples):
+        self.principle_components = e_vec[:, 0:self.H]  # First H components
+        self.e_val = e_val
+        self.e_vec = e_vec
 
-        centered_samples = test_samples - self.sample_mean
+    def gram_pca_train(self, samples, p, h=0):
 
-        self.transformed_data = np.dot(self.principle_components.T, centered_samples.T)
+        self.type = 'Gram'
 
-        pass
+        centering_matrix = np.identity(len(samples)) - (1/len(samples))*np.ones((len(samples), len(samples)))
+
+        centered_sample_matrix = np.dot(centering_matrix, samples)
+        self.gram_matrix = np.dot(centered_sample_matrix, centered_sample_matrix.T)
+
+        double_centered_gram = centering_matrix @ self.gram_matrix @ centering_matrix
+
+        e_val, e_vec = sorted_eigen(double_centered_gram)
+
+        if h == 0:
+            sum = e_val[0]
+            stop_val = p * np.trace(self.gram_matrix)
+            h = 0
+            while sum < stop_val:
+                h += 1
+                sum += e_val[h]
+
+        self.H = h
+
+        print(self.H)
+
+        self.principle_components = np.dot(np.dot(centering_matrix, e_vec[:, 0:self.H]), \
+                                           np.linalg.inv(np.sqrt(np.diag(e_val[0:self.H]))))
+
+        print(self.principle_components.shape)
+
+        self.e_vec = e_vec
+        self.e_val = e_val
+
+    def encode(self, test_samples, training_samples=None):
+        """
+
+        :param training_samples:
+        :param test_samples:
+        :return:
+        """
+
+        if self.type == 'PCA':
+            centered_samples = test_samples - self.sample_mean
+            self.transformed_data = np.dot(centered_samples, self.principle_components)
+
+        elif self.type == "Gram":
+            test_vs_training_gram = np.dot(test_samples, training_samples.T)
+            centered_tvt_gram = test_vs_training_gram - (1/len(training_samples))*self.gram_matrix
+            self.transformed_data = np.dot(centered_tvt_gram, self.principle_components)
 
     def decode(self):
+        """
 
-        self.decoded_data = np.dot(self.transformed_data.T, self.principle_components.T).astype(dtype=float) \
-                            + self.sample_mean
+        :return:
+        """
+        if self.type == 'PCA':
+            self.decoded_data = np.dot(self.transformed_data, self.principle_components.T).astype(dtype=float) \
+                                + self.sample_mean
 
-        pass
+        elif self.type == "Gram":
+            print(self.transformed_data.shape)
+            self.decoded_data = self.principle_components @ self.transformed_data.T
+
+
+    def plot_cumulative_explained_variance(self):
+        """
+        Plots the cumulative explained variance vs the Number of components
+        """
+        plt.stem(np.cumsum(self.e_val / np.sum(self.e_val)))
+        plt.axis('tight')
+        plt.grid()
+        plt.xlabel('Number of Components')
+        plt.ylabel('Cumulative Explained Variance')
+        plt.show()
 
 
 def sorted_eigen(matrix):
+    """
+    Finds the Eigen values and vectors, and reorders them in descending order
+    :param matrix: Matrix that will be EVD
+    :return: Eigen value and Eigen vector in descending order
+    """
     e_val, e_vec = np.linalg.eig(matrix)
 
     index = e_val.argsort()[::-1]
--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 from sklearn.datasets import load_digits
 from sklearn.datasets import fetch_openml
 from sklearn.datasets import fetch_lfw_people
+import numpy as np
 import matplotlib.pyplot as plt
 import autoencoder
 from random import sample
 import idx2numpy
+from PIL import Image
+from pathlib import Path
 
 
 # TODO:
 #   Fix PCA
 #   Make getting samples better
 #   Better display
-#
+#   Add Faces
 
 
-if __name__ == "__main__":
+def load_8x8_digits():
+    """
+    Loads 8 8x8 digits
+    :return: Training Samples, Training Labels, Test Samples, Test Labels
+    """
+    digits = load_digits()  # 8x8 digits
+    training_samples = digits.data / 255
+    training_y = np.array(digits.target, dtype=int)
+    test_samples = training_samples.copy()
+    test_y = training_y.copy()
+    image_shape = (8, 8)
 
-    # Simple 8x8 digts
-    # digits = load_digits()  # 8x8 digits
-    # X = digits.data / 255
-    # Y = np.array(digits.target, dtype=int)
+    return training_samples, training_y, test_samples, test_y, image_shape
 
+
+def load_28x28_digits():
+    """
+    Loads 60000 28x28 digits for training.
+    Loads 10000 28x28 digits for testing.
+    :return: Training Samples, Training Labels, Test Samples, Test Labels, Image Shape
+    """
     # 28x28 bit digits
     training_samples = idx2numpy.convert_from_file('samples/train-images.idx3-ubyte')
-    Y = idx2numpy.convert_from_file('samples/train-labels.idx1-ubyte')
+    training_y = idx2numpy.convert_from_file('samples/train-labels.idx1-ubyte')
 
-    training_samples = training_samples.reshape(len(training_samples), training_samples[0].size)
-    training_samples = training_samples[:] / 255
+    image_shape = training_samples[0].shape
 
+    training_samples = training_samples.reshape(len(training_samples), training_samples[0].size)  # Samples in one vector
+    training_samples = training_samples[:] / 255  # Normalize between 0 and 1
 
     test_samples = idx2numpy.convert_from_file('samples/t10k-images.idx3-ubyte')
     test_y = idx2numpy.convert_from_file('samples/t10k-labels.idx1-ubyte')
@@ -35,17 +53,71 @@ if __name__ == "__main__":
     test_samples = test_samples.reshape(len(test_samples), test_samples[0].size)
     test_samples = test_samples[:] / 255
 
+    return training_samples, training_y, test_samples, test_y, image_shape
 
+
+def load_faces():
+
+    if Path('samples/faces/face_data.npy').is_file():
+        samples = np.load('samples/faces/face_data.npy', allow_pickle=True)
+    else:
+        filelist = []
+        path = 'samples/faces/subject'
+        for i in range(1,16):
+            mod = ''
+            if i < 10:
+                mod = '0'
+            filelist.append(path + mod + str(i) + '.centerlight')
+            filelist.append(path + mod + str(i) + '.glasses')
+            filelist.append(path + mod + str(i) + '.happy')
+            filelist.append(path + mod + str(i) + '.leftlight')
+            filelist.append(path + mod + str(i) + '.noglasses')
+            filelist.append(path + mod + str(i) + '.normal')
+            filelist.append(path + mod + str(i) + '.rightlight')
+            filelist.append(path + mod + str(i) + '.sad')
+            filelist.append(path + mod + str(i) + '.sleepy')
+            filelist.append(path + mod + str(i) + '.surprised')
+            filelist.append(path + mod + str(i) + '.wink')
+
+
+        samples = np.array(([np.array(Image.open(fname)) for fname in filelist]))
+
+        samples.dump('samples/faces/face_data.npy')  # Save samples so don't have to read each time
+
+    samples = samples[:, 0:samples[0].shape[0], 0:samples[0].shape[1]]  # downsample by 4
+
+    image_shape = samples[0].shape
+
+    samples = samples.reshape(len(samples), samples[0].size)
+
+    return samples, image_shape
+
+
+if __name__ == "__main__":
+
+    # training_samples, training_y, test_samples, test_y, image_shape = load_8x8_digits()
+
+    samples, image_shape = load_faces()
+
+    # plt.imshow(samples[0].reshape(image_shape), cmap='gray')
+    # plt.show()
+
+    # Create Auto encoder
     auto = autoencoder.autoencoder()
 
-    auto.pca_train(training_samples, 0.5)
-
-    auto.encode(test_samples)
-
+    auto.gram_pca_train(samples, 0.9)
+    auto.encode(samples, samples)
     auto.decode()
+    #
+    # f = plt.figure()
+    #
+    # for i in range(2):  # show 8 examples
+    #     plt.imshow(samples[i].reshape(image_shape), cmap='gray')
+    #     plt.show()
+    #     plt.imshow(auto.decoded_data[i].reshape(image_shape), cmap='gray')
+    #     plt.show()
 
-    for i in range(8):  # show 8 examples
-        print("This is supposed to be a '", test_y[i], "':", sep="")
-        plt.imshow(auto.decoded_data[i].reshape([28, 28]))
-        plt.show()
+    auto.plot_cumulative_explained_variance()
+
+
 
