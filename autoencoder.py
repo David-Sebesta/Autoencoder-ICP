119debf35a11b305454f8f0ca28b18126f746246
David-Sebesta-FIT <64850214+David-Sebesta-FIT@users.noreply.github.com>
2021-11-27 22:28:47 -0500

    Fixed PCA and added documentation

--- autoencoder.py
+++ autoencoder.py
@@ -1,32 +1,55 @@
+"""
+Autoencoder ICP
+David Sebesta
+Pattern Recognition
+Fall 2021
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 class autoencoder:
+    """
+    An autoencoder using PCA
+    """
 
     def __init__(self):
+        """
+        Creates a basic (linear) autoencoder using PCA.
+        """
         self.H = 0  # Number of principle components
 
-        self.principle_components = None
-        self.sample_mean = None
-        self.transformed_data = None
-        self.decoded_data = None
+        self.principle_components = None  # Numpy array of principle component
+        self.sample_mean = None  # Sample mean
+        self.transformed_data = None  # Encoded or transformed data
+        self.decoded_data = None  # Decoded Data
 
-        self.e_val = None
-        self.e_vec = None
+        self.e_val = None  # Eigen Values
+        self.e_vec = None  # Eigen Vectors
 
-        self.type = None
+        self.type = None  # PCA (Covariance Based) or Gram
 
-        pass
-
-    def pca_train(self, samples, p, h=0):
+    def pca_train(self, samples, p, h=None):
         """
-        :param samples:
+        Finds the primary components using the covariance method. \n
+
+        .. math::
+            \\hat{\mu}_{x} = \\frac{1}{N}X^{T}1_{N}
+
+            \\tilde{X} = X - 1_{N}\\hat{\mu}_{x}
+
+            \\hat{C}_{x} = \\frac{1}{N}\\tilde{X}^{T}\\tilde{X}
+
+            [\\nu , \\lambda] = EVD(\\hat{C}_{x})
+
+            H = arg\\min\{k\\in\{1,...,D\}:\\sum_{i=1}^{k} \ \\lambda_{i} \\geq ptrace\{\\hat{C}_{x}\}\}
+
+        :param samples: Training Samples
         :param p: Total Variance Explanied between 0 and 1
-        :param h
+        :param h: Number of principle components wanted to be used. If no input, it is calculated based of p
         """
 
-        self.type = 'PCA'
+        self.type = 'PCA'  # Set the type of Autoencoder to PCA
 
         self.sample_mean = samples.mean()  # Compute Sample mean
         centered_samples = samples - self.sample_mean  # Center samples around mean
@@ -36,85 +59,110 @@ class autoencoder:
         e_val, e_vec = sorted_eigen(sample_cov)  # Eigen Decomposition of sample covariance matrix
         # They have to be sorted in descending order
 
-        if h == 0:
+        if h == None:
             sum = e_val[0]
-            stop_val = p * np.trace(sample_cov)
+            stop_val = p * np.trace(sample_cov)  # Total Variance explained * trace of the sample covariance matrix
             h = 0
-            while sum < stop_val:
-                h += 1
-                sum += e_val[h]
+            while sum < stop_val:               # When the sum of eigen values is greater than
+                h += 1                          # the Total Variance explained * trace of the sample cov matrix
+                sum += e_val[h]                  # That is when the number of principle components is found
 
-        self.H = h
-
-        print("The number of principle components is:", self.H)
+        self.H = h  # Set the number of primary components
+        print("The number of principle components is:", self.H + 1)
 
         self.principle_components = e_vec[:, 0:self.H]  # First H components
         self.e_val = e_val
         self.e_vec = e_vec
 
     def gram_pca_train(self, samples, p, h=0):
+        """
+        Finds the primary components using the gram matrix method.
 
-        self.type = 'Gram'
+        :param samples: Training Samples
+        :param p: Total Variance Explanied between 0 and 1
+        :param h: Number of principle components wanted to be used. If no input, it is calculated based of p
+        """
 
-        centering_matrix = np.identity(len(samples)) - (1/len(samples))*np.ones((len(samples), len(samples)))
+        self.type = 'Gram'  # Set the type of autoencoder to Gram
 
-        centered_sample_matrix = np.dot(centering_matrix, samples)
+        centering_matrix = np.identity(len(samples)) - (1 / len(samples)) * np.ones((len(samples), len(samples)))
+
+        centered_sample_matrix = centering_matrix @ samples
         self.gram_matrix = np.dot(centered_sample_matrix, centered_sample_matrix.T)
 
         double_centered_gram = centering_matrix @ self.gram_matrix @ centering_matrix
 
         e_val, e_vec = sorted_eigen(double_centered_gram)
 
-        if h == 0:
+        if h == None:
             sum = e_val[0]
-            stop_val = p * np.trace(self.gram_matrix)
+            stop_val = p * np.trace(self.gram_matrix)   # Total Variance explained * trace of the gram matrix
             h = 0
-            while sum < stop_val:
-                h += 1
-                sum += e_val[h]
+            while sum < stop_val:                       # When the sum of eigen values is greater than
+                h += 1                                  # the Total Variance explained * trace of the gram matrix
+                sum += e_val[h]                         # That is when the number of principle components is found
 
         self.H = h
 
-        print(self.H)
+        print("The number of principle components is:", self.H + 1)
 
-        self.principle_components = np.dot(np.dot(centering_matrix, e_vec[:, 0:self.H]), \
-                                           np.linalg.inv(np.sqrt(np.diag(e_val[0:self.H]))))
-
-        print(self.principle_components.shape)
+        self.principle_components = centering_matrix @ e_vec[:, 0:self.H] @ np.linalg.inv(
+            np.sqrt(np.diag(e_val[0:self.H])))
 
         self.e_vec = e_vec
         self.e_val = e_val
 
     def encode(self, test_samples, training_samples=None):
         """
+        Encodes the test samples use the primary components. If Covariance style PCA is used, training samples does not
+        need to be added. \n
 
-        :param training_samples:
-        :param test_samples:
-        :return:
+        Covariance PCA:
+
+        .. math::
+            \\tilde{X}_{t} = X_t - 1_{M}\\hat{\\mu}^{T}_{x}
+
+            \\tilde{Y}_{t} = \\tilde{X}_{t}\\nu_{H}
+
+        If gram style PCA is used, training samples needs to be inputed otherwise the test samples will be used. \n
+
+
+        :param test_samples: The samples that will be encoded
+        :param training_samples: The samples used for training. Only needed from Gram PCA.
         """
 
         if self.type == 'PCA':
-            centered_samples = test_samples - self.sample_mean
-            self.transformed_data = np.dot(centered_samples, self.principle_components)
+            centered_samples = test_samples - self.sample_mean.T
+            self.transformed_data = test_samples @ self.principle_components  # Since the test samples are row vectors
+                                                                              # no transpose is needed
 
         elif self.type == "Gram":
-            test_vs_training_gram = np.dot(test_samples, training_samples.T)
-            centered_tvt_gram = test_vs_training_gram - (1/len(training_samples))*self.gram_matrix
-            self.transformed_data = np.dot(centered_tvt_gram, self.principle_components)
+            if training_samples is None:
+                training_samples = test_samples
+            test_vs_training_gram = test_samples @ training_samples.T
+            centered_tvt_gram = test_vs_training_gram - (1 / len(training_samples)) * self.gram_matrix
+            self.transformed_data = centered_tvt_gram @ self.principle_components
 
     def decode(self):
         """
+        Decodes the transformed data. \n
+        Covariance PCA:
 
-        :return:
+        .. math::
+            \\hat{X}_{t} = \\tilde{Y}_{t}\\nu^{T}_{H} + \\hat{\\mu}_{x}
+
+
+        Gram PCA:
+            Unknown
+
+
         """
         if self.type == 'PCA':
             self.decoded_data = np.dot(self.transformed_data, self.principle_components.T).astype(dtype=float) \
-                                + self.sample_mean
+                                + self.sample_mean.T
 
         elif self.type == "Gram":
-            print(self.transformed_data.shape)
             self.decoded_data = self.principle_components @ self.transformed_data.T
-
 
     def plot_cumulative_explained_variance(self):
         """
@@ -130,7 +178,8 @@ class autoencoder:
 
 def sorted_eigen(matrix):
     """
-    Finds the Eigen values and vectors, and reorders them in descending order
+    Finds the Eigen values and vectors, and reorders them in descending order.
+
     :param matrix: Matrix that will be EVD
     :return: Eigen value and Eigen vector in descending order
     """
@@ -138,6 +187,6 @@ def sorted_eigen(matrix):
 
     index = e_val.argsort()[::-1]
     e_val = e_val[index]
-    e_vec = e_vec[index]
+    e_vec = e_vec[:, index]
 
     return e_val, e_vec
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+"""
+Autoencoder ICP
+David Sebesta
+Pattern Recognition
+Fall 2021
+"""
+
 from sklearn.datasets import load_digits
-from sklearn.datasets import fetch_openml
-from sklearn.datasets import fetch_lfw_people
 import numpy as np
 import matplotlib.pyplot as plt
 import autoencoder
@@ -11,113 +16,225 @@ from pathlib import Path
 
 
 # TODO:
-#   Fix PCA
-#   Make getting samples better
-#   Better display
-#   Add Faces
+#   Seperate the samples from 8x8 into training and testing
 
 
 def load_8x8_digits():
     """
+    Uses the mnist 8x8 digits
     Loads 8 8x8 digits
-    :return: Training Samples, Training Labels, Test Samples, Test Labels
+    :return:
+        Training Samples: NxD matrix of training samples
+        Training Labels: N length vector of samples labels
+        Test Samples: MxD matrix of testing samples
+        Test Labels: M length vector of training labels
+        Image Shape: Shape of image (8, 8)
     """
-    digits = load_digits()  # 8x8 digits
-    training_samples = digits.data / 255
-    training_y = np.array(digits.target, dtype=int)
-    test_samples = training_samples.copy()
-    test_y = training_y.copy()
-    image_shape = (8, 8)
+
+    digits = load_digits()                          # 8x8 digits from sklearn dataset
+    training_samples = digits.data / 255            # Scale data
+    training_y = np.array(digits.target, dtype=int) # Get training labels
+    test_samples = training_samples.copy()          # Test samples are the same the training ## will be changed ##
+    test_y = training_y.copy()                      # Get test labels
+    image_shape = (8, 8)                            # Image shape is 8x8
 
     return training_samples, training_y, test_samples, test_y, image_shape
 
 
 def load_28x28_digits():
     """
+    Uses the mnist 28x28 digits.
     Loads 60000 28x28 digits for training.
     Loads 10000 28x28 digits for testing.
-    :return: Training Samples, Training Labels, Test Samples, Test Labels, Image Shape
+    :return:
+        Training Samples: NxD matrix of training samples
+        Training Labels: N length vector of samples labels
+        Test Samples: MxD matrix of testing samples
+        Test Labels: M length vector of training labels
+        Image Shape: Shape of image (28, 28)
     """
-    # 28x28 bit digits
-    training_samples = idx2numpy.convert_from_file('samples/train-images.idx3-ubyte')
-    training_y = idx2numpy.convert_from_file('samples/train-labels.idx1-ubyte')
 
-    image_shape = training_samples[0].shape
+    training_samples = idx2numpy.convert_from_file('samples/train-images.idx3-ubyte')   # Load mnist training samples
+    training_y = idx2numpy.convert_from_file('samples/train-labels.idx1-ubyte')         # Load mnist training labels
 
-    training_samples = training_samples.reshape(len(training_samples), training_samples[0].size)  # Samples in one vector
+    image_shape = training_samples[0].shape  # Finds the shape of the images
+
+    training_samples = training_samples.reshape(len(training_samples), training_samples[0].size)  # Reshapes to NxD
     training_samples = training_samples[:] / 255  # Normalize between 0 and 1
 
-    test_samples = idx2numpy.convert_from_file('samples/t10k-images.idx3-ubyte')
-    test_y = idx2numpy.convert_from_file('samples/t10k-labels.idx1-ubyte')
+    test_samples = idx2numpy.convert_from_file('samples/t10k-images.idx3-ubyte')        # Load mnist testing samples
+    test_y = idx2numpy.convert_from_file('samples/t10k-labels.idx1-ubyte')              # Load mnist testing labels
 
-    test_samples = test_samples.reshape(len(test_samples), test_samples[0].size)
-    test_samples = test_samples[:] / 255
+    test_samples = test_samples.reshape(len(test_samples), test_samples[0].size)                # Reshapes to MxD
+    test_samples = test_samples[:] / 255    # Normalizes between 0 and 1
 
     return training_samples, training_y, test_samples, test_y, image_shape
 
 
-def load_faces():
-
-    if Path('samples/faces/face_data.npy').is_file():
-        samples = np.load('samples/faces/face_data.npy', allow_pickle=True)
+def load_cropped_faces(downsample=4):
+    """
+    Loads cropped yale faces. If file doesnt exists, it creates one.
+    :param downsample: Amount the images will be downsampled
+    :return:
+        samples: The images in an N x D matrix, where N is the number of images and D is the number of pixels
+        images_shape: The shape of the image to be used to reshape the matricies to plot
+    """
+    if Path('samples/cropped_faces/face_data.npy').is_file():
+        samples = np.load('samples/cropped_faces/cropped_face_data.npy', allow_pickle=True)
     else:
         filelist = []
-        path = 'samples/faces/subject'
-        for i in range(1,16):
-            mod = ''
-            if i < 10:
-                mod = '0'
-            filelist.append(path + mod + str(i) + '.centerlight')
-            filelist.append(path + mod + str(i) + '.glasses')
-            filelist.append(path + mod + str(i) + '.happy')
-            filelist.append(path + mod + str(i) + '.leftlight')
-            filelist.append(path + mod + str(i) + '.noglasses')
-            filelist.append(path + mod + str(i) + '.normal')
-            filelist.append(path + mod + str(i) + '.rightlight')
-            filelist.append(path + mod + str(i) + '.sad')
-            filelist.append(path + mod + str(i) + '.sleepy')
-            filelist.append(path + mod + str(i) + '.surprised')
-            filelist.append(path + mod + str(i) + '.wink')
 
+        for i in range(1, 166):
+            filelist.append('samples/cropped_faces/sample' + str(i) + '.gif')
+            samples = np.array(([np.array(Image.open(fname)) for fname in filelist]))
 
-        samples = np.array(([np.array(Image.open(fname)) for fname in filelist]))
+        samples = samples[:,:,:,0]
+        samples.dump('samples/cropped_faces/copped_face_data.npy')  # Save samples so don't have to read each time
 
-        samples.dump('samples/faces/face_data.npy')  # Save samples so don't have to read each time
-
-    samples = samples[:, 0:samples[0].shape[0], 0:samples[0].shape[1]]  # downsample by 4
+    samples = samples[:, 0:samples[0].shape[0]:downsample, 0:samples[0].shape[1]:downsample]  # downsample by 8
 
     image_shape = samples[0].shape
 
     samples = samples.reshape(len(samples), samples[0].size)
-
+    samples = samples[:] / 255
     return samples, image_shape
 
 
-if __name__ == "__main__":
+def add_noise(samples, strength=0.5):
+    """
+    Add a uniform noise to the signal.
 
-    # training_samples, training_y, test_samples, test_y, image_shape = load_8x8_digits()
+    :param samples: Samples
+    :param strength: Max amplitude of noise
+    :return:
+        Samples added with a uniform noise from 0 to 1
+    """
+    noise = np.random.uniform(0, 1, samples[0].size)
+    return samples[:] + noise * strength
 
-    samples, image_shape = load_faces()
 
-    # plt.imshow(samples[0].reshape(image_shape), cmap='gray')
-    # plt.show()
+def train_8x8_digits(p=0.9, h=None, noise_strength=0, n_images=2, plot_cev=False):
+    """
+    Built in autoencoder training and image showing. Uses mnist 8x8 digits. Change noise strength to a value between 0
+    and 1 to add noise to the test samples.
+
+    :param p: Total Variance Explained to be used
+    :param h: Number of primary components wanted to be used. If left blank or None, it is found using p.
+    :param noise_strength: Strength of the noise added to the test signal. Use a value between 0 and 1. Recommended
+    between 0.001 and 0.1 anything greater will not be recognizable
+    :param n_images: Amount of images to be shown
+    :param plot_cev: If true, plots the Cumulative Explained Variance vs Number of principle components
+    """
+
+    training_samples, training_y, test_samples, test_y, image_shape = load_8x8_digits()
+
+    auto = autoencoder.autoencoder()
+    auto.pca_train(samples=training_samples, p=p, h=h)
+
+    if noise_strength != 0:
+        test_samples = add_noise(test_samples, strength=noise_strength)
+
+    auto.encode(test_samples=test_samples)
+    auto.decode()
+
+    random_indexes = sample(range(0, len(test_samples)), n_images)
+
+    for i in random_indexes:
+        plt.subplot(1, 2, 1)
+        plt.title('Test Sample: ' + str(test_y[i]))
+        plt.imshow(test_samples[i].reshape(image_shape), cmap='gray')
+        plt.subplot(1, 2, 2)
+        plt.title('Decoded Sample: ' + str(test_y[i]))
+        plt.imshow(auto.decoded_data[i].reshape(image_shape), cmap='gray')
+        plt.show()
+
+    if plot_cev:
+        auto.plot_cumulative_explained_variance()
+
+
+def train_28x28_digits(p=0.9, h=None, noise_strength=0, n_images=2, plot_cev=False):
+    """
+    Built in autoencoder training and image showing. Uses mnist 28x28 digits. Change noise strength to a value between 0
+    and 1 to add noise to the test samples.
+
+    :param p: Total Variance Explained to be used
+    :param h: Number of primary components wanted to be used. If left blank or None, it is found using p.
+    :param noise_strength: Strength of the noise added to the test signal. Use a value between 0 and 1.
+    :param n_images: Amount of images to be shown
+    :param plot_cev: If true, plots the Cumulative Explained Variance vs Number of principle components
+    """
+    training_samples, training_y, test_samples, test_y, image_shape = load_28x28_digits()
+
+    auto = autoencoder.autoencoder()
+    auto.pca_train(samples=training_samples, p=p, h=h)
+
+    if noise_strength != 0:
+        test_samples = add_noise(test_samples, strength=noise_strength)
+
+    auto.encode(test_samples=test_samples)
+    auto.decode()
+
+    random_indexes = sample(range(0, len(test_samples)), n_images)
+
+    for i in random_indexes:
+        plt.subplot(1, 2, 1)
+        plt.title('Test Sample: ' + str(test_y[i]))
+        plt.imshow(test_samples[i].reshape(image_shape), cmap='gray')
+        plt.subplot(1, 2, 2)
+        plt.title('Decoded Sample: ' + str(test_y[i]))
+        plt.imshow(auto.decoded_data[i].reshape(image_shape), cmap='gray')
+        plt.show()
+
+    if plot_cev:
+        auto.plot_cumulative_explained_variance()
+
+
+def train_faces(p=0.9, h=None, noise_strength=0, n_images=2, plot_cev=False, downsample=4):
+    """
+    Built in autoencoder and image showing. PCA trains an autoencoder using the cropped and downscaled yale faces.
+    Change noise strength to a value between 0 and 1 to add noise to the test samples.
+
+    :param p: Total Variance Explained to be used
+    :param h: Number of primary components wanted to be used. If left blank or None, it is found using p.
+    :param noise_strength: Strength of the noise added to the test signal. Use a value between 0 and 1.
+    :param n_images: Amount of images to be shown
+    :param plot_cev: If true, plots the Cumulative Explained Variance vs Number of principle components
+    :param downsample: amount the images will be downsampled (needs to be at least 3, not enough ram)
+    """
+
+    samples, image_shape = load_cropped_faces(downsample=downsample)
 
     # Create Auto encoder
     auto = autoencoder.autoencoder()
 
-    auto.gram_pca_train(samples, 0.9)
-    auto.encode(samples, samples)
-    auto.decode()
-    #
-    # f = plt.figure()
-    #
-    # for i in range(2):  # show 8 examples
-    #     plt.imshow(samples[i].reshape(image_shape), cmap='gray')
-    #     plt.show()
-    #     plt.imshow(auto.decoded_data[i].reshape(image_shape), cmap='gray')
-    #     plt.show()
+    auto.pca_train(samples=samples, p=p, h=h)
 
-    auto.plot_cumulative_explained_variance()
+    if noise_strength != 0:
+        samples = add_noise(samples, strength=noise_strength)
+
+    auto.encode(test_samples=samples)
+    auto.decode()
+
+    random_indexes = sample(range(0, len(samples)), n_images)
+
+    for i in random_indexes:
+        plt.subplot(1, 2, 1)
+        plt.title('Test Sample')
+        plt.imshow(samples[i].reshape(image_shape), cmap='gray')
+        plt.subplot(1, 2, 2)
+        plt.title('Decoded Sample')
+        plt.imshow(auto.decoded_data[i].reshape(image_shape), cmap='gray')
+        plt.show()
+
+    if plot_cev:
+        auto.plot_cumulative_explained_variance()
+
+
+if __name__ == "__main__":
+
+    train_8x8_digits()
+
+
 
 
 
